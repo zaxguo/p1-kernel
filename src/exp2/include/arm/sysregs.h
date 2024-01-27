43087ddb51ee12495293743a290b4e9d0815f1a1
Felix Lin <linxz02@gmail.com>
2024-01-27 11:19:51 -0500

    minor code change

--- src/exp2/include/arm/sysregs.h
+++ src/exp2/include/arm/sysregs.h
@@ -19,14 +19,14 @@
 // HCR_EL2, Hypervisor Configuration Register (EL2), Page 2487 of AArch64-Reference-Manual.
 // ***************************************
 
-#define HCR_RW	    			(1 << 31)
+#define HCR_RW	    		(1 << 31)
 #define HCR_VALUE			HCR_RW
 
 // ***************************************
 // SCR_EL3, Secure Configuration Register (EL3), Page 2648 of AArch64-Reference-Manual.
 // ***************************************
 
-#define SCR_RESERVED	    		(3 << 4)
+#define SCR_RESERVED	    (3 << 4)
 #define SCR_RW				(1 << 10)
 #define SCR_NS				(1 << 0)
 #define SCR_VALUE	    	    	(SCR_RESERVED | SCR_RW | SCR_NS)
@@ -35,7 +35,7 @@
 // SPSR_EL3, Saved Program Status Register (EL3) Page 389 of AArch64-Reference-Manual.
 // ***************************************
 
-#define SPSR_MASK_ALL 			(7 << 6)
+#define SPSR_MASK_ALL 		(7 << 6)
 #define SPSR_EL1h			(5 << 0)
 #define SPSR_VALUE			(SPSR_MASK_ALL | SPSR_EL1h)
 
