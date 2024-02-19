fc936c05e30e82c237b128e7915f2d71f407153a
Felix Lin <linxz02@gmail.com>
2024-02-18 21:38:59 -0500

    fixed S_FRAME_SIZE

--- src/exp4a/src/entry.h
+++ src/exp4a/src/entry.h
@@ -1,7 +1,7 @@
 #ifndef _ENTRY_H
 #define _ENTRY_H
 
-#define S_FRAME_SIZE			272 		// size of all saved registers 
+#define S_FRAME_SIZE			256 		// size of all saved registers (no elr/spsr saved)
 
 #define SYNC_INVALID_EL1t		0 
 #define IRQ_INVALID_EL1t		1 
